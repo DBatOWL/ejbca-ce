a3210199cc267c1662e38b4ad85baaea98d119ea
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2022-01-28 14:48:21 +0100

    ECA-10518: revert update to log4j2 api

--- modules/cesecore-ejb-interface/src/org/cesecore/audit/impl/log4j/Log4jDevice.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/audit/impl/log4j/Log4jDevice.java
@@ -14,14 +14,14 @@ package org.cesecore.audit.impl.log4j;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.ErrorHandler;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.ErrorHandler;
 import org.cesecore.audit.AuditLogDevice;
 import org.cesecore.audit.AuditLogEntry;
 import org.cesecore.audit.audit.AuditExporter;
@@ -46,113 +46,113 @@ import org.cesecore.util.query.QueryCriteria;
  * 
  * This implementation does not comply to the CESeCore TSF regarding log protection and only provided as a compliment
  * to monitor operations or for use in high performance environments where compliance is not an issue.
+ * 
+ * @version $Id$
  */
 public class Log4jDevice implements AuditLogDevice {
 
-    private static final Logger LOG = LogManager.getLogger(Log4jDevice.class);
-	private static final String UNSUPPORTED = Log4jDevice.class.getSimpleName() + " does not support query, verification or export operations.";
-	private final List<Log4jDeviceErrorHandler> errorHandlers = new ArrayList<Log4jDeviceErrorHandler>();
-	
-	public Log4jDevice() {
-	    if (LOG instanceof org.apache.logging.log4j.core.Logger) {
-    		final Map<String, Appender> appendersMap = ((org.apache.logging.log4j.core.Logger) LOG).getAppenders();
-            for (final Appender appender : appendersMap.values()) {
-                final ErrorHandler errorHandler = appender.getHandler();
-                if (errorHandler != null) {
-                    final Log4jDeviceErrorHandler wrappedErrorHandler = new Log4jDeviceErrorHandler(errorHandler);
-                    errorHandlers.add(wrappedErrorHandler);
-                    appender.setHandler(wrappedErrorHandler);
+    private static final Logger LOG = Logger.getLogger(Log4jDevice.class);
+    private static final String UNSUPPORTED = Log4jDevice.class.getSimpleName() + " does not support query, verification or export operations.";
+    private final List<Log4jDeviceErrorHandler> errorHandlers = new ArrayList<Log4jDeviceErrorHandler>();
+    
+    public Log4jDevice() {
+        @SuppressWarnings("unchecked")
+        final Enumeration<Appender> enumeration = LOG.getAllAppenders();
+        while (enumeration.hasMoreElements()) {
+            final Appender appender = enumeration.nextElement();
+            final ErrorHandler errorHandler = appender.getErrorHandler();
+            if (errorHandler != null) {
+                final Log4jDeviceErrorHandler wrappedErrorHandler = new Log4jDeviceErrorHandler(errorHandler);
+                errorHandlers.add(wrappedErrorHandler);
+                appender.setErrorHandler(wrappedErrorHandler);
+            }
+        }
+    }
+    
+    private void assertNoErrors() throws AuditRecordStorageException {
+        for (final Log4jDeviceErrorHandler errorHandler : errorHandlers) {
+            if (!errorHandler.isOk()) {
+                throw new AuditRecordStorageException("A log4j device failed to log.");
+            }
+        }
+    }
+
+    @Override
+    public boolean isSupportingQueries() {
+        return false;
+    }
+
+    @Override
+    public void setEjbs(final Map<Class<?>, ?> ejbs) {
+        // Does not use any beans
+    }
+
+    @Override
+    public AuditLogExportReport exportAuditLogs(final AuthenticationToken token, final CryptoToken cryptoToken, final Date timestamp,
+            final boolean deleteAfterExport, final Map<String, Object> signatureDetails, final Properties properties, final Class<? extends AuditExporter> c) throws AuditLogExporterException {
+        throw new UnsupportedOperationException(UNSUPPORTED);
+    }
+
+    @Override
+    public List<? extends AuditLogEntry> selectAuditLogs(final AuthenticationToken token, final int startIndex, final int max, final QueryCriteria criteria, final Properties properties) {
+        throw new UnsupportedOperationException(UNSUPPORTED);
+    }
+
+    @Override
+    public AuditLogValidationReport verifyLogsIntegrity(final AuthenticationToken token, final Date date, final Properties properties) throws AuditLogValidatorException {
+        throw new UnsupportedOperationException(UNSUPPORTED);
+    }
+
+    @Override
+    public void log(final TrustedTime trustedTime, final EventType eventType, final EventStatus eventStatus, final ModuleType module, final ServiceType service,
+            final String authToken, final String customId, final String searchDetail1, final String searchDetail2, final Map<String, Object> additionalDetails, Properties properties)
+            throws AuditRecordStorageException {
+        // Log lines are usually between 117 and 1700 bytes. An initial length of 1024 will cover most of them, speeding things up.
+        final StringBuilder sb = new StringBuilder(1024);
+        if (trustedTime != null) {
+            sb.append(ValidityDate.formatAsISO8601(trustedTime.getTime(), ValidityDate.TIMEZONE_SERVER));
+        }
+        appendIfNotNull(sb, eventType);
+        appendIfNotNull(sb, eventStatus);
+        appendIfNotNull(sb, module);
+        appendIfNotNull(sb, service);
+        appendIfNotNull(sb, authToken);
+        appendIfNotNull(sb, customId);
+        appendIfNotNull(sb, searchDetail1);
+        appendIfNotNull(sb, searchDetail2);
+        if (additionalDetails != null) {
+            for (final String detail : additionalDetails.keySet()) {
+                if (sb.length()!=0) {
+                    sb.append(';');
+                }
+                sb.append(detail).append('=');
+                final Object o = additionalDetails.get(detail);
+                if (o != null) {
+                    sb.append(o.toString());
                 }
             }
-	    } else {
-	        LOG.warn("Failed to add error handler to appender. Logger class: " + LOG.getClass().getName());
-	    }
-	}
-	
-	private void assertNoErrors() throws AuditRecordStorageException {
-		for (final Log4jDeviceErrorHandler errorHandler : errorHandlers) {
-			if (!errorHandler.isOk()) {
-				throw new AuditRecordStorageException("A log4j device failed to log.");
-			}
-		}
-	}
+        }
+        LOG.info(sb.toString());
+        assertNoErrors();
+    }
+    
+    // TODO: Not perfect for parsing, since any of the appended values might contain a ';' char
+    private void appendIfNotNull(final StringBuilder sb, final Object o) {
+        if (sb.length()!=0) {
+            sb.append(';');
+        }
+        if (o != null) {
+            sb.append(o.toString());
+        }
+    }
 
-	@Override
-	public boolean isSupportingQueries() {
-		return false;
-	}
+    @Override
+    public void prepareReset() throws AuditLogResetException {
+        // No action required
+    }
 
-	@Override
-	public void setEjbs(final Map<Class<?>, ?> ejbs) {
-		// Does not use any beans
-	}
-
-	@Override
-	public AuditLogExportReport exportAuditLogs(final AuthenticationToken token, final CryptoToken cryptoToken, final Date timestamp,
-			final boolean deleteAfterExport, final Map<String, Object> signatureDetails, final Properties properties, final Class<? extends AuditExporter> c) throws AuditLogExporterException {
-		throw new UnsupportedOperationException(UNSUPPORTED);
-	}
-
-	@Override
-	public List<? extends AuditLogEntry> selectAuditLogs(final AuthenticationToken token, final int startIndex, final int max, final QueryCriteria criteria, final Properties properties) {
-		throw new UnsupportedOperationException(UNSUPPORTED);
-	}
-
-	@Override
-	public AuditLogValidationReport verifyLogsIntegrity(final AuthenticationToken token, final Date date, final Properties properties) throws AuditLogValidatorException {
-		throw new UnsupportedOperationException(UNSUPPORTED);
-	}
-
-	@Override
-	public void log(final TrustedTime trustedTime, final EventType eventType, final EventStatus eventStatus, final ModuleType module, final ServiceType service,
-			final String authToken, final String customId, final String searchDetail1, final String searchDetail2, final Map<String, Object> additionalDetails, Properties properties)
-			throws AuditRecordStorageException {
-	    // Log lines are usually between 117 and 1700 bytes. An initial length of 1024 will cover most of them, speeding things up.
-		final StringBuilder sb = new StringBuilder(1024);
-		if (trustedTime != null) {
-			sb.append(ValidityDate.formatAsISO8601(trustedTime.getTime(), ValidityDate.TIMEZONE_SERVER));
-		}
-		appendIfNotNull(sb, eventType);
-		appendIfNotNull(sb, eventStatus);
-		appendIfNotNull(sb, module);
-		appendIfNotNull(sb, service);
-		appendIfNotNull(sb, authToken);
-		appendIfNotNull(sb, customId);
-		appendIfNotNull(sb, searchDetail1);
-		appendIfNotNull(sb, searchDetail2);
-		if (additionalDetails != null) {
-			for (final String detail : additionalDetails.keySet()) {
-				if (sb.length()!=0) {
-					sb.append(';');
-				}
-				sb.append(detail).append('=');
-				final Object o = additionalDetails.get(detail);
-				if (o != null) {
-					sb.append(o.toString());
-				}
-			}
-		}
-		LOG.info(sb.toString());
-		assertNoErrors();
-	}
-	
-	// TODO: Not perfect for parsing, since any of the appended values might contain a ';' char
-	private void appendIfNotNull(final StringBuilder sb, final Object o) {
-		if (sb.length()!=0) {
-			sb.append(';');
-		}
-		if (o != null) {
-			sb.append(o.toString());
-		}
-	}
-
-	@Override
-	public void prepareReset() throws AuditLogResetException {
-		// No action required
-	}
-
-	@Override
-	public void reset() throws AuditLogResetException {
-		// No action required
-	}
+    @Override
+    public void reset() throws AuditLogResetException {
+        // No action required
+    }
 }
--- modules/cesecore-ejb-interface/src/org/cesecore/audit/impl/log4j/Log4jDeviceErrorHandler.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/audit/impl/log4j/Log4jDeviceErrorHandler.java
@@ -12,30 +12,34 @@
  *************************************************************************/
 package org.cesecore.audit.impl.log4j;
 
-import org.apache.logging.log4j.core.ErrorHandler;
-import org.apache.logging.log4j.core.LogEvent;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.ErrorHandler;
+import org.apache.log4j.spi.LoggingEvent;
 
 /**
  * Custom wrapper for Log4J ErrorHandler, so we can check if logging was successful or not.
+ * 
+ * @version $Id$
  */
 public class Log4jDeviceErrorHandler implements ErrorHandler {
-	
-	private boolean ok = true;
-	
-	private final ErrorHandler errorHandler;
-	
-	public Log4jDeviceErrorHandler(final ErrorHandler errorHandler) {
-		this.errorHandler = errorHandler;
-	}
-	
-	/** @return true if this error handler has not been invoked since last call to this method. */
-	public boolean isOk() {
-		if (ok) {
-			return true;
-		}
-		ok = true;	// Reset
-		return false;
-	}
+    
+    private boolean ok = true;
+    
+    private final ErrorHandler errorHandler;
+    
+    public Log4jDeviceErrorHandler(final ErrorHandler errorHandler) {
+        this.errorHandler = errorHandler;
+    }
+    
+    /** @return true if this error handler has not been invoked since last call to this method. */
+    public boolean isOk() {
+        if (ok) {
+            return true;
+        }
+        ok = true;  // Reset
+        return false;
+    }
 
     @Override
     public void error(final String arg0) {
@@ -44,15 +48,35 @@ public class Log4jDeviceErrorHandler implements ErrorHandler {
     }
 
     @Override
-    public void error(String msg, Throwable t) {
-        errorHandler.error(msg, t);
+    public void error(final String arg0, final Exception arg1, final int arg2) {
+        errorHandler.error(arg0, arg1, arg2);
         ok = false;
-        
     }
 
     @Override
-    public void error(String msg, LogEvent event, Throwable t) {
-        errorHandler.error(msg, event, t);
-        ok = false;        
+    public void error(final String arg0, final Exception arg1, final int arg2, final LoggingEvent arg3) {
+        errorHandler.error(arg0, arg1, arg2, arg3);
+        ok = false;
+    }
+
+    @Override
+    public void setAppender(final Appender arg0) {
+        errorHandler.setAppender(arg0);
+    }
+
+    @Override
+    public void setBackupAppender(final Appender arg0) {
+        errorHandler.setBackupAppender(arg0);
+    }
+
+    @Override
+    public void setLogger(final Logger arg0) {
+        errorHandler.setLogger(arg0);
+    }
+
+    // EJBCAINTER-323 Removed.
+//  @Override
+    public void activateOptions() {
+//      errorHandler.activateOptions();
     }
 }
